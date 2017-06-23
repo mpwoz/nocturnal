ce771a4c3a2093f7a7c8c8b515a4cc1f6cfc0cf8
Martin Wozniewicz <martin.woz@gmail.com>
2017-06-23 08:44:28 -0700

    source maps support and uncompressed css for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 // taken from http://danbahrami.io/articles/building-a-production-website-with-hugo-and-gulp-js/
 
 var gulp         = require("gulp"),
+    sourcemaps   = require("gulp-sourcemaps"),
     sass         = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer")
 
 // Compile SCSS files to CSS
 gulp.task("scss", function () {
-    gulp.src("src/scss/*.scss")
+    gulp.src("src/scss/**/*.scss")
         .pipe(sass({
             outputStyle : "compressed"
         }))
@@ -16,11 +17,23 @@ gulp.task("scss", function () {
         .pipe(gulp.dest("static/css"))
 })
 
-// Watch asset folder for changes
-gulp.task("watch", ["scss"], function () {
-    gulp.watch("src/scss/*", ["scss"])
+// Compile SCSS files to CSS
+gulp.task("scss-dev", function () {
+    gulp.src("src/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("static/css"))
 })
 
+// Watch asset folder for changes
+gulp.task("watch", ["scss"], function () {
+    gulp.watch("src/scss/**/*", ["scss"])
+})
+
+gulp.task("watch-dev", ["scss-dev"], function () {
+    gulp.watch("src/scss/**/*", ["scss-dev"])
+})
 
 // Set watch as default task
-gulp.task("default", ["watch"])
+gulp.task("default", ["watch-dev"])
